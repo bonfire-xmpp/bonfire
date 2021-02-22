e0dab96a264e448878e0ae33d4210c3415955d12
Upkeep <upkeep@airmail.cc>
2021-02-22 19:58:26 +0100

    Explicit component import removes deferred load, which fixes an issue where SystemBar would render *after* page load

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,3 +6,12 @@
     </v-main>
   </v-app>
 </template>
+
+<script>
+  import SystemBar from "@/components/SystemBar";
+
+  export default {
+    layout: 'default',
+    components: {SystemBar}
+  }
+</script>
