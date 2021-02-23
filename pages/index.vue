b22d2e5fd7f12b8a36249b69f5732963827bc1bc
Upkeep <upkeep@airmail.cc>
2021-02-23 01:33:42 +0100

    Added an empty / route

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script>
+  import { Store } from '@/store';
+  import { mapState } from 'vuex';
+
+  export default {
+    name: "index",
+    computed: {
+      ...mapState({
+        loggedIn: Store.$states.loggedIn,
+      })
+    },
+
+    mounted() {
+      if(!this.loggedIn) this.$router.push('/login');
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
