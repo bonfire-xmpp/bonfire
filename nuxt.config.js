cfbc6b6314bbbdb797ca9e7c40e7ba2b0c92c459
Upkeep <upkeep@airmail.cc>
2021-02-27 00:42:10 +0100

    feat(styling): add JS and SASS variables for color scheme & theming

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
-import colors from 'vuetify/es5/util/colors'
+// import colors from 'vuetify/es5/util/colors'
+import colors from './assets/colors.js';
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
@@ -72,16 +73,7 @@ export default {
     theme: {
       dark: true,
       themes: {
-        dark: {
-          "grey-100": "#1e1e1e",
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
+        dark: colors
       }
     }
   },
