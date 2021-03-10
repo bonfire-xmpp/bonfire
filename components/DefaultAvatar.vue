72f83dc322e1312776ff93d7b334bcf17620f3e3
Upkeep <upkeep@airmail.cc>
2021-03-10 22:01:02 +0100

    feat(avatars): add default avatar

---
+++ components/DefaultAvatar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+      :style="{ height: size+'px', width: size+'px', backgroundColor: color + ` !important` }"
+      class="rounded-circle black"
+  >
+    <v-icon class="w-100 h-100">mdi-account</v-icon>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "DefaultAvatar",
+
+    props: {
+      size: Number,
+      color: {
+        type: String,
+        optional: true,
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
